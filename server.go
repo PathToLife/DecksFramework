61d772b62951c1c9319406dd67eedc2ca997a13b
Sean Z <pathtolifegithub@gmail.com>
2021-06-21 06:59:28 +1200

    refactor server.go, use net/http for status codes

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"decksframework/decks"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"strconv"
 	"strings"
 )
@@ -48,60 +49,60 @@ func initializeRoutes() {
 		// ?cards=AS,2S,KH
 		if cardCodesStr, isCustom := c.GetQuery("cards"); isCustom {
 			if cardCodesStr == "" {
-				c.AbortWithStatusJSON(400, ErrorMsg("cards requires comma separated list of card codes"))
+				c.AbortWithStatusJSON(http.StatusBadRequest, ErrorMsg("cards requires comma separated list of card codes"))
 				return
 			}
 			cardCodes := strings.Split(cardCodesStr, ",")
 			customDeck, err := decks.NewStandardPartialDeck(cardCodes, shouldShuffleDeck)
 			if err != nil {
-				c.AbortWithStatusJSON(500, ErrorMsg(err.Error()))
+				c.AbortWithStatusJSON(http.StatusBadRequest, ErrorMsg(err.Error()))
 				return
 			}
 			deck = customDeck
-		} else { // standard deck fi cards is not specified
+		} else { // standard deck if ?cards= is not specified
 			standardDeck, err := decks.NewStandardDeck(shouldShuffleDeck)
 			if err != nil {
-				_ = c.AbortWithError(500, err)
+				_ = c.AbortWithError(http.StatusInternalServerError, err)
 				return
 			}
 			deck = standardDeck
 		}
 
 		deckStore.AddDeck(deck)
-		c.JSON(201, deck.GetClosedDeck())
+		c.JSON(http.StatusCreated, deck.GetClosedDeck())
 	})
 
 	router.POST("/deck/draw", func(c *gin.Context) {
 		uuid, ok := c.GetQuery("uuid")
 		if !ok || uuid == "" {
-			c.AbortWithStatusJSON(400, ErrorMsg("uuid required"))
+			c.AbortWithStatusJSON(http.StatusBadRequest, ErrorMsg("uuid required"))
 			return
 		}
 
 		countStr, ok := c.GetQuery("count")
 		if !ok {
-			c.AbortWithStatusJSON(400, ErrorMsg("count required"))
+			c.AbortWithStatusJSON(http.StatusBadRequest, ErrorMsg("count required"))
 			return
 		}
 		drawCount, err := strconv.Atoi(countStr)
 		if err != nil {
-			c.AbortWithStatusJSON(400, ErrorMsg("count needs to be a number"))
+			c.AbortWithStatusJSON(http.StatusBadRequest, ErrorMsg("count needs to be a number"))
 			return
 		}
 
 		deck := deckStore.GetDeck(uuid)
 		if deck == nil {
-			c.AbortWithStatusJSON(404, ErrorMsg("deck not found"))
+			c.AbortWithStatusJSON(http.StatusNotFound, ErrorMsg("deck not found"))
 			return
 		}
 
 		cards, err := deck.DrawCard(drawCount)
 		if err != nil {
-			c.AbortWithStatusJSON(400, ErrorMsg(err.Error()))
+			c.AbortWithStatusJSON(http.StatusBadRequest, ErrorMsg(err.Error()))
 			return
 		}
 
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"cards": cards,
 		})
 	})
@@ -109,17 +110,17 @@ func initializeRoutes() {
 	router.GET("/deck/open", func(c *gin.Context) {
 		uuid, ok := c.GetQuery("uuid")
 		if !ok || uuid == "" {
-			c.AbortWithStatusJSON(400, ErrorMsg("uuid required"))
+			c.AbortWithStatusJSON(http.StatusBadRequest, ErrorMsg("uuid required"))
 			return
 		}
 
 		deck := deckStore.GetDeck(uuid)
 		if deck == nil {
-			c.AbortWithStatusJSON(404, ErrorMsg("deck not found"))
+			c.AbortWithStatusJSON(http.StatusNotFound, ErrorMsg("deck not found"))
 			return
 		}
 
-		c.JSON(200, deck.GetOpenDeck())
+		c.JSON(http.StatusOK, deck.GetOpenDeck())
 	})
 }
 
